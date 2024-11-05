5d903f150b84562006e8b99ea5712e978c4bac8f
Alya Atlab <133522435+alya-atlab@users.noreply.github.com>
2024-11-05 14:01:26 +0200

    Create the Function

--- move_zeros.py
+++ move_zeros.py
@@ -1,1 +1,16 @@
+def move_zeros(numbers):
+    zeros = 0
+    new_list = []
+    for i in numbers:
+        if i == 0:
+            print(i)
+            zeros += 1
+        else:
+            new_list.append(i)
+    print(zeros)
+    for i in range (zeros):
+        new_list.append(0)
+    return new_list
 
+print(move_zeros([0,1,3,0,0,0,0,0,4,5,0,0,1,2,10,0,0]))
+    
